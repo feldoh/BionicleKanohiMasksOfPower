82f10d3a6c5dbcbc39bd58bf62cab02af802bbf7
Taranchuk <nfhfyxer@gmail.com>
2021-12-21 22:28:57 +0200

    bugfixes

--- 1.3/Source/BionicleKanohiMasksOfPower/Patch_MakeRecipeProducts.cs
+++ 1.3/Source/BionicleKanohiMasksOfPower/Patch_MakeRecipeProducts.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using RimWorld;
 using System.Collections.Generic;
+using System.Linq;
 using Verse;
 
 namespace BionicleKanohiMasksOfPower
@@ -13,25 +14,28 @@ namespace BionicleKanohiMasksOfPower
     [HarmonyPatch("MakeRecipeProducts")]
     public static class Patch_MakeRecipeProducts
     {
-        public static void Postfix(ref IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker, List<Thing> ingredients, Thing dominantIngredient, IBillGiver billGiver)
+        public static IEnumerable<Thing> Postfix(IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker, List<Thing> ingredients, Thing dominantIngredient, IBillGiver billGiver)
         {
-            if (recipeDef.workerClass == typeof(Recipe_TransferQuality))
+            var qualities = new List<QualityCategory>();
+            foreach (var i in ingredients)
             {
-                foreach (var i in ingredients)
+                if (i.TryGetQuality(out QualityCategory qc))
                 {
-                    if (i.TryGetQuality(out QualityCategory qc))
+                    qualities.Add(qc);
+                }
+            }
+
+            foreach (var r in __result)
+            {
+                if (qualities.Any() && recipeDef.workerClass == typeof(Recipe_TransferQuality))
+                {
+                    var comp = r.TryGetComp<CompQuality>();
+                    if (comp != null)
                     {
-                        foreach (var t in __result)
-                        {
-                            var comp = t.TryGetComp<CompQuality>();
-                            if (comp != null)
-                            {
-                                comp.SetQuality(qc, ArtGenerationContext.Colony);
-                                return;
-                            }
-                        }
+                        comp.SetQuality(qualities.Max(), ArtGenerationContext.Colony);
                     }
                 }
+                yield return r;
             }
         }
 
